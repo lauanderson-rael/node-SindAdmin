e75834ac2e82854315af00ba1b3fd742e0a66336
Lauanderson Rael <100454015+lauanderson-rael@users.noreply.github.com>
2025-05-06 19:27:08 -0300

    Update server.mjs removido body parser

--- server.mjs
+++ server.mjs
@@ -8,7 +8,7 @@ import { PDFDocument } from 'pdf-lib';
 // Configurações
 const app = express();
 const port = 3000;
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/templates', express.static('templates'));
 
